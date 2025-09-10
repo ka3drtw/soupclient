161248ce7892735a2755e2082587f457969cb78b
Ka3dr <ka3drofficial@gmail.com>
2025-09-10 17:50:11 +0300

    freeze_kill.h

--- src/game/client/gameclient.h
+++ src/game/client/gameclient.h
@@ -60,6 +60,7 @@
 #include "components/soup/bindchat.h"
 #include "components/soup/bindwheel.h"
 #include "components/soup/bg_draw.h"
+#include "components/soup/freeze_kill.h"
 #include "components/soup/playerpics.h"
 #include "components/soup/outlines.h"
 #include "components/soup/player_indicator.h"
