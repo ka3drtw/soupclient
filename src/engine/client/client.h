726d4f5e2b0769e69771a8d385f8068ea35e93d7
Ka3dr <ka3drofficial@gmail.com>
2025-09-12 20:44:40 +0300

    Update client.h

--- src/engine/client/client.h
+++ src/engine/client/client.h
@@ -289,6 +289,7 @@ public:
 	// Send via the currently active client (main/dummy)
 	int SendMsgActive(CMsgPacker *pMsg, int Flags) override;
 
+	void SendSoupInfo(int Conn);
 	void SendInfo(int Conn);
 	void SendEnterGame(int Conn);
 	void SendReady(int Conn);
